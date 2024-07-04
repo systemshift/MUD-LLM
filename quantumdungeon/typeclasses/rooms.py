e2f2290101f8ad53ded36112486ca9a8c9292f7f
systemshift <42102034+systemshift@users.noreply.github.com>
2024-07-04 07:02:12 +0300

    init evennia game with a single player

---
+++ quantumdungeon/typeclasses/rooms.py
@@ -0,0 +1,29 @@
+"""
+Room
+
+Rooms are simple containers that has no location of their own.
+
+"""
+
+from evennia.objects.objects import DefaultRoom
+
+from .objects import ObjectParent
+
+
+class Room(ObjectParent, DefaultRoom):
+    """
+    Rooms are like any Object, except their location is None
+    (which is default). They also use basetype_setup() to
+    add locks so they cannot be puppeted or picked up.
+    (to change that, use at_object_creation instead)
+
+    See mygame/typeclasses/objects.py for a list of
+    properties and methods available on all Objects.
+    """
+
+    pass
+
+class DungeonRoom(DefaultRoom):
+    def at_object_creation(self):
+        super().at_object_creation()
+        self.db.desc = "You are in a dark, damp dungeon room. A fearsome monster lurks in the shadows."
